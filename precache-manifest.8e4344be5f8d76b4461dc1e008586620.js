246c39ef75ae496ee9748f6cb71e0d481d001c6a
Bhavin Dhalwani <dhalwanibhavin@gmail.com>
2020-06-30 21:21:34 +0530

    Updates

---
+++ precache-manifest.8e4344be5f8d76b4461dc1e008586620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca18a35ef3fa6f77c70a6939de64d2e9",
+    "url": "/BhavinReactApp/index.html"
+  },
+  {
+    "revision": "2ad584311778a3b7df45",
+    "url": "/BhavinReactApp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fa18331b0b446e3666ac",
+    "url": "/BhavinReactApp/static/css/main.bd46b3d2.chunk.css"
+  },
+  {
+    "revision": "2ad584311778a3b7df45",
+    "url": "/BhavinReactApp/static/js/2.078fd67f.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/BhavinReactApp/static/js/2.078fd67f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa18331b0b446e3666ac",
+    "url": "/BhavinReactApp/static/js/main.239d230c.chunk.js"
+  },
+  {
+    "revision": "0f26a121ab739b5b9bf0",
+    "url": "/BhavinReactApp/static/js/runtime-main.33387ec8.js"
+  }
+]);
